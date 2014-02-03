f4817325e33871f241e05a0b18a4bff2e46e804f
Florian Eckerstorfer <florian@theroadtojoy.at>
2014-02-04 00:01:33 +0100

    Updated prices

--- data/venues.php
+++ data/venues.php
@@ -9,7 +9,7 @@
       'name' => 'salon wichtig',
       'description' => 'Great vegan and vegetarian curry to go. Only open for lunch',
       'address' => 'Karlsgasse 22, 1040 Wien',
-      'price' => 4, # The number of dollar signs
+      'price' => 1, # The number of dollar signs
       'best_for' => 'Lunch', # "Lunch" or "Dinner". Can be anything.
       'url' => 'http://salonwichtig.com', # An external URL
       'type' => 'Curry', # The category of the venue. Diner, sushi bar, cafe, etc
@@ -18,7 +18,7 @@
       'name' => 'The Highlander Pub &amp; Garden',
       'description' => 'Austrian food and great beer. It\'s best when you can sit outside, so come in summer.',
       'address' => 'Sobieskiplatz 4, 1090 Wien',
-      'price' => 3,
+      'price' => 2,
       'best_for' => 'Dinner',
       'url' => 'http://www.the-highlander.at',
       'type' => 'Austrian',
